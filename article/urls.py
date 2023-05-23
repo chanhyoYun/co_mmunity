ff73738c1b16fd3763115820ce5c65293add5fd1
newstyle1443 <131249376+newstyle1443@users.noreply.github.com>
2023-05-23 14:37:36 +0900

    게시글, 댓글 완성

--- article/urls.py
+++ article/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:article_id>/comments/", views.Comments.as_view()), #특정 게시글에 코멘트 작성.
-    path("<int:article_id>/comments/<comment_id>/", views.DetailComments.as_view()) # 특정 코멘트 수정, 삭제
+    path("<int:article_id>/comments/", views.CommentsView.as_view()), #특정 게시글에 코멘트 작성.
+    path("<int:article_id>/comments/<comment_id>/", views.DetailComments.as_view()), # 특정 코멘트 수정, 삭제
+    path("", views.ArticlesView.as_view()), # 게시글 작성
+    path("<int:article_id>/",views.ArticleDetailView.as_view()), # 게시글 수정 삭제
 ]
