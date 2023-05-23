98b44ca13a5921e938895b702e7aee4ee3a0704d
madonghwi <ehd0831@naver.com>
2023-05-23 11:09:10 +0900

    post 추가

--- users/views.py
+++ users/views.py
@@ -10,6 +10,13 @@ from rest_framework.permissions import IsAuthenticated
 class SignupView(APIView):
     serializer_class = SignupSerializer
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class WithdrawView(APIView):
     permission_classes = [IsAuthenticated]
 
